d43c7af8fa9f5030d1b8311e258e22a154e6d619
BenjaminBakija <benjaminbakija20@gmail.com>
2019-03-20 09:42:20 +0100

    Added hello world and css changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label helloWorld;
+
+    public void SayHello(ActionEvent actionEvent) {
+        helloWorld.setText("Hello world");
+    }
 }
